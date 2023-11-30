df8a9720155e505a02f9b581486319939d387fdf
edods <146672024+edods@users.noreply.github.com>
2023-11-30 12:28:07 -0500

    Update visualizations.js

--- visualizations.js
+++ visualizations.js
@@ -2,7 +2,7 @@ d3.csv("Visualization1DataNew.csv").then(
 
     function(dataset){
 
-        console.log(dataset)
+        //console.log(dataset)
 
        var dimensions = {
             width: 700,
@@ -29,7 +29,7 @@ d3.csv("Visualization1DataNew.csv").then(
                 }
         )
 
-        console.log(yMax)
+        //console.log(yMax)
         
         var yMin = d3.min(dataset, function(d){
             var min = Math.min(+d.le_agg_F, +d.le_agg_M);
@@ -37,7 +37,7 @@ d3.csv("Visualization1DataNew.csv").then(
             }
         )
             
-        console.log(yMin)
+        //console.log(yMin)
 
         var yScale = d3.scaleLinear()
                        .domain([yMin, yMax])
@@ -46,16 +46,23 @@ d3.csv("Visualization1DataNew.csv").then(
         const line1 = d3.line()
                         .x(d => xScale(+d.pctile))
                         .y(d => yScale(+d.le_agg_F))
+                        //.attr("id", "line_f")
 
         const line2 = d3.line()
                         .x(d => xScale(+d.pctile))
                         .y(d => yScale(+d.le_agg_M))
+                        //.attr("id", "line_m")
+        
+        // var tooltip = d3.select("#linechart")
+        //                 .append("div")
+        //                 .style("position", "absolute")
+        //                 .style("visibility", "hidden")
+        //                 .text("I'm a circle!");
 
         svg.append("g")
             .attr("transform", `translate(0,${dimensions.height - dimensions.margin.bottom})`)
-            .call(d3.axisBottom(xScale).ticks(dimensions.width / 80).tickSizeOuter(0));
+            .call(d3.axisBottom(xScale).ticks(20).tickSizeOuter(0));
            
-
         svg.append("g")
            .attr("transform", `translate(${dimensions.margin.left},0)`)
            .call(d3.axisLeft(yScale).ticks(dimensions.height / 40))
@@ -69,6 +76,7 @@ d3.csv("Visualization1DataNew.csv").then(
                .attr("text-anchor", "start"));
 
         svg.append("path")
+            .attr("id", "line_f")
             .attr("fill", "none")
             .attr("stroke", "red")
             .attr("stroke-width", 1.5)
@@ -76,13 +84,28 @@ d3.csv("Visualization1DataNew.csv").then(
             .on('mouseover', function(d){
                 d3.select(this)
                   .style("stroke-width", 4.0)
+                //tooltip.style("visibility", "visible")
                })
-               .on('mouseout', function(d){
+            .on('mouseout', function(d){
                 d3.select(this)
                   .style("stroke-width", 1.5)
-               })
+            })
+            // .on('click', function(d){
+            //     d3.select("#line_f")
+            //       .style("stroke-width", 4.0)
+            // })
+            .on('click', function(event, d){
+              let gender = "F"
+              const xCoor = d3.pointer(event)[0];
+            update_map(xCoor, gender)
+            console.log('you clicked female')
+            //d3.selectAll(".state")//.attr("fill", d => colorScale(+state_le[d.properties.NAME]))
+
+            })
+            //.on('click', update_map_f(d))
 
         svg.append("path")
+            .attr("id", "line_m")
             .attr("fill", "none")
             .attr("stroke", "steelblue")
             .attr("stroke-width", 1.5)
@@ -91,18 +114,27 @@ d3.csv("Visualization1DataNew.csv").then(
                 d3.select(this)
                   .style("stroke-width", 4.0)
                })
-               .on('mouseout', function(d){
+            .on('mouseout', function(d){
                 d3.select(this)
                   .style("stroke-width", 1.5)
-               })
-    }  
+            })
+            .on('click', function(event, d){
+              let gender = "M"
+              const xCoor = d3.pointer(event)[0];
+            update_map(xCoor, gender)
+            console.log('you clicked male')
+            //d3.selectAll(".state")//.attr("fill", d => colorScale(+state_le[d.properties.NAME]))
+
+            })
+
+  }  
 )
 
 d3.csv("Visualization2Data.csv").then(
 
     function(dataset){
 
-        console.log(dataset)
+        //console.log(dataset)
 
        var dimensions = {
             width: 1400,
@@ -127,10 +159,7 @@ d3.csv("Visualization2Data.csv").then(
 
         var svg = d3.select("#scatterplot")
                     .style("width", dimensions.width)
-                    .style("height", dimensions.height)
-
-
-                    
+                    .style("height", dimensions.height)    
 
       var yMax = d3.max(dataset, function(d){
             var max = Math.max(+d.le_agg_slope_q1_F, +d.le_agg_slope_q2_F, +d.le_agg_slope_q3_F, +d.le_agg_slope_q4_F,
@@ -140,7 +169,6 @@ d3.csv("Visualization2Data.csv").then(
             }
         )
     
-
         console.log(yMax)
         
         var yMin = d3.min(dataset, function(d){
@@ -151,24 +179,22 @@ d3.csv("Visualization2Data.csv").then(
             }
         )
 
-        console.log(yMin)
+        //console.log(yMin)
 
         var xScale = d3.scaleBand()
                        .domain(d3.map(dataset, d => d.stateabbrv))
                        .range([dimensions.margin.left, dimensions.width - dimensions.margin.right])
 
-        console.log(d3.map(dataset, d => d.stateabbrv))
-
-
-
+        //console.log(d3.map(dataset, d => d.stateabbrv))
 
         var yScale = d3.scaleLinear()
                        .domain([yMin, yMax])
                        .range([dimensions.height - dimensions.margin.bottom, dimensions.margin.top])
       
-        console.log([yMin, yMax])
+        //console.log([yMin, yMax])
 
         var f_q1 = svg.append("g")
+                      .attr("id", "point_f_q1")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -192,7 +218,8 @@ d3.csv("Visualization2Data.csv").then(
                           .attr("r", 4)
                        })
 
-        var f_q2 = svg.append("g")
+       var f_q2 = svg.append("g")
+                      .attr("id", "point_f_q2")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -217,6 +244,7 @@ d3.csv("Visualization2Data.csv").then(
                        })
 
         var f_q3 = svg.append("g")
+                      .attr("id", "point_f_q3")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -241,6 +269,7 @@ d3.csv("Visualization2Data.csv").then(
                        })
 
         var f_q4 = svg.append("g")
+                      .attr("id", "point_f_q4")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -265,6 +294,7 @@ d3.csv("Visualization2Data.csv").then(
                        })
 
         var m_q1 = svg.append("g")
+                      .attr("id", "point_m_q1")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -289,6 +319,7 @@ d3.csv("Visualization2Data.csv").then(
                        })
 
         var m_q2 = svg.append("g")
+                      .attr("id", "point_m_q2")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -313,6 +344,7 @@ d3.csv("Visualization2Data.csv").then(
                        })
 
         var m_q3 = svg.append("g")
+                      .attr("id", "point_m_q3")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -337,6 +369,7 @@ d3.csv("Visualization2Data.csv").then(
                        })
 
         var m_q4 = svg.append("g")
+                      .attr("id", "point_m_q4")
                       .selectAll("circle")
                       .data(dataset)
                       .enter()
@@ -360,10 +393,7 @@ d3.csv("Visualization2Data.csv").then(
                           .attr("r", 4)
                        })
 
-
-
         var xAxisGen = d3.axisBottom().scale(xScale)
-
 
         var xAxis = svg.append("g")
                        .call(xAxisGen)
@@ -383,6 +413,21 @@ d3.csv("Visualization2Data.csv").then(
                        .call(yAxisGen)
                        .style("transform", `translateX(${dimensions.margin.left}px)`)
                        //.text("State-level Estimates of Trends in Life Expectancy")
+        // d3.select("#q1_f").on('click', function(){
+        //   console.log('clicked')
+        //   dataset.filter(function(d) {
+        //     if (d.id != "point_f_q1"){
+        //       d3.select(this)
+        //         .style("opacity", 1.0)
+        //     }
+        //     else{
+        //       d3.select(this)
+        //         .attr("stroke", "black")
+        //         .style("stroke-width", 2.0)
+        //         .attr("r", 6)
+        //     }
+        //   })
+      //})
     }  
 )
 
@@ -392,13 +437,13 @@ d3.csv("Visualization3Data.csv").then(
 
         d3.json("us_states.json").then(function(mapdata){
 
-            console.log(dataset)
-            console.log(mapdata)
+            //console.log(dataset)
+            //console.log(mapdata)
 
             var state_le = {}
             dataset.forEach(d => 
                 {state_le[d["statename"]] = +d["combined_le_FM"]}) 
-            console.log(state_le)
+            //console.log(state_le)
 
             var w = 700;
             var h = 350;
@@ -415,12 +460,13 @@ d3.csv("Visualization3Data.csv").then(
 
             var colorScale = d3.scaleLinear()
                                .domain([d3.min(Object.values(state_le)), d3.max(Object.values(state_le))])
-                               .range(["lightblue", "blue"])
+                               .range(["#e4c6ca", "purple"])
 
             var mapGroup = svg.append("g")
                               .attr("transform", "translate(" + w / 4 + "," + h / 300 + ")");
             
             var states = mapGroup.selectAll(".state")
+                               .attr("id", "states")
                                .data(mapdata.features)
                                .enter()
                                .append("path")
@@ -442,3 +488,107 @@ d3.csv("Visualization3Data.csv").then(
         })
     }  
 )
+
+function update_map(xCoor, gender){
+  
+  d3.csv("Visualization3Data.csv").then(
+  
+    function(dataset){
+  
+      var state_le = {}
+
+        d3.json("us_states.json").then(function(mapdata){
+          console.log(gender)
+          console.log(xCoor)
+            //console.log(dataset)
+            //console.log(mapdata)
+          if (gender == "F"){
+            if (xCoor < 160) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q1_F"]}) 
+            }
+            else if (xCoor < 347 && xCoor >= 160) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q2_F"]}) 
+            }
+            else if (xCoor < 497 && xCoor >= 347) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q3_F"]}) 
+            }
+            else if (xCoor < 649 && xCoor >= 497) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q4_F"]}) 
+            }
+          }
+
+          if (gender == "M"){
+            if (xCoor < 160) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q1_M"]}) 
+            }
+            else if (xCoor < 347 && xCoor >= 160) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q2_M"]}) 
+            }
+            else if (xCoor < 497 && xCoor >= 347) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q3_M"]}) 
+            }
+            else if (xCoor < 649 && xCoor >= 497) {
+              dataset.forEach(d => 
+                {state_le[d["statename"]] = +d["le_agg_q4_M"]}) 
+            }
+          }
+  
+            var w = 700;
+            var h = 350;
+  
+            var svg = d3.select("#map")
+                        .attr("width", w)
+                        .attr("height", h)
+    
+            var projection = d3.geoAlbersUsa()  
+                               .translate([w/3 , h/2]) // Adjust these values
+                               .scale([w]);  
+            
+            var path = d3.geoPath().projection(projection);
+  
+            if (gender == "F"){
+              var colorScale = d3.scaleLinear()
+                                 .domain([d3.min(Object.values(state_le)), d3.max(Object.values(state_le))])
+                                 .range(["white", "red"])
+            }
+
+            if (gender == "M"){
+              var colorScale = d3.scaleLinear()
+                                 .domain([d3.min(Object.values(state_le)), d3.max(Object.values(state_le))])
+                                 .range(["lightblue", "blue"])
+            }
+  
+            var mapGroup = svg.append("g")
+                              .attr("transform", "translate(" + w / 4 + "," + h / 300 + ")");
+            
+            var states = mapGroup.selectAll(".state")
+                               .attr("id", "states")
+                               .data(mapdata.features)
+                               .enter()
+                               .append("path")
+                               .attr("class", "state")
+                               .attr("d", d => path(d))
+                               .attr("fill", d => colorScale(+state_le[d.properties.NAME]))
+                               .attr("stroke", "black")
+                               .style("stroke-width", 0.3)
+                               .on('mouseover', function(d){
+                                d3.select(this)
+                                  .attr("stroke", "black")
+                                  .style("stroke-width", 2.0)
+                               })
+                               .on('mouseout', function(d){
+                                d3.select(this)
+                                  .attr("stroke", "black")
+                                  .style("stroke-width", 0.3)
+                               })
+        })
+    }  
+  )
+  }
